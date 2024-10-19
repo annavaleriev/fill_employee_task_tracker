34df228fd96db8c38547a76292dc192e32e5b3fb
annavaleriev <filenko.a@gmail.com>
2024-10-19 19:06:39 +0300

    Приписаны urls

--- employee_tasks/urls.py
+++ employee_tasks/urls.py
@@ -1,12 +1,12 @@
 from rest_framework.routers import DefaultRouter
 
 from employee_tasks.apps import EmployeeTasksConfig
-from employee_tasks.views import EmployeeViewSet
+from employee_tasks.views import EmployeeViewSet, TaskViewSet
 
 app_name = EmployeeTasksConfig.name
 
 router = DefaultRouter()
 router.register("employee", EmployeeViewSet)
-# router.register("task", TaskViewSet)
+router.register("task", TaskViewSet)
 
 urlpatterns = router.urls
