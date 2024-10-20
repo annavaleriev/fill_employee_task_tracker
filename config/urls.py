64c95b9748bfaccedcf4d98c8823572a26fc255c
annavaleriev <filenko.a@gmail.com>
2024-10-20 19:59:29 +0300

    Прописаны urls

--- config/urls.py
+++ config/urls.py
@@ -4,8 +4,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('employee_tasks/', include('employee_tasks.urls'), name='employee_tasks'),
-    path("", include("docs")),
+    path('', include('employee_tasks.urls'), name='employee_tasks'),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="docs"),
 
--- employee_tasks/urls.py
+++ employee_tasks/urls.py
@@ -1,7 +1,10 @@
+from django.urls import path
+from rest_framework.permissions import AllowAny
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from employee_tasks.apps import EmployeeTasksConfig
-from employee_tasks.views import EmployeeViewSet, TaskViewSet
+from employee_tasks.views import EmployeeViewSet, TaskViewSet, EmployeeCreateView
 
 app_name = EmployeeTasksConfig.name
 
@@ -9,4 +12,13 @@ router = DefaultRouter()
 router.register("employee", EmployeeViewSet)
 router.register("task", TaskViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+                  path(
+                      "login/",
+                      TokenObtainPairView.as_view(permission_classes=(AllowAny,)),
+                      name="login",
+                  ),
+                  path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+                  path("register/", EmployeeCreateView.as_view(), name="register"),
+
+              ] + router.urls
