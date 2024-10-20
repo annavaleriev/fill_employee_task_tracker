990ce443e6d096e01abeca927524798d3d5c0a19
annavaleriev <filenko.a@gmail.com>
2024-10-20 20:00:49 +0300

    Написаны сериализаторы

--- employee_tasks/serializer.py
+++ employee_tasks/serializer.py
@@ -3,14 +3,32 @@ from rest_framework import serializers
 from employee_tasks.models import Employee, Task
 
 
-class EmployeeSerializer(serializers.ModelSerializer):
-    """Сериализатор для сотрудников"""
-    class Meta:
-        model = Employee
-        fields = '__all__'
-
 class TaskSerializer(serializers.ModelSerializer):
     """Сериализатор для задач"""
     class Meta:
         model = Task
         fields = '__all__'
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    """Сериализатор для сотрудников"""
+    active_tasks_count = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Employee
+        fields = ("id", "email", "first_name", "last_name", "position", "active_tasks_count")
+
+
+class BusyEmployeeSerializer(serializers.ModelSerializer):
+    """Сериализатор для сотрудников"""
+    task_set = TaskSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Employee
+        fields = ("id", "email", "first_name", "last_name", "position", "task_set")
+
+
+class EmployeeCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = ("password", "email", "first_name", "last_name", "position")
