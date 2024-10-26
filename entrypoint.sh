e79d268a633d81325fab6c3b3cd54dd7b5f2791e
annavaleriev <filenko.a@gmail.com>
2024-10-26 17:06:17 +0300

    Обновлен файл entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-
-# Прекращаем выполнение при ошибках
-set -e
-
-# Проверка миграций и их применение
-echo "Применение миграций базы данных"
+#
+## Прекращаем выполнение при ошибках
+#set -e
+#
+## Проверка миграций и их применение
+#echo "Применение миграций базы данных"
 #python manage.py migrate
 #
 ## Создание суперпользователя, если он не существует
@@ -19,6 +19,39 @@ echo "Применение миграций базы данных"
 #else:
 #    print('Суперпользователь уже существует')
 #END
+#
+## Запуск сервера
+#python manage.py runserver 0.0.0.0:8000
+#exec "$@"
+#!/bin/sh
+
+# Прекращаем выполнение при ошибках
+set -e
+
+# Проверка миграций и их применение
+echo "Применение миграций базы данных"
+python manage.py migrate
+
+# Создание суперпользователя, если он не существует
+echo "Создание суперпользователя, если его нет"
+
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+if email and password:
+    if not User.objects.filter(email=email).exists():
+        User.objects.create_superuser(email=email, password=password)
+        print('Суперпользователь создан')
+    else:
+        print('Суперпользователь уже существует')
+else:
+    print('Переменные окружения для суперпользователя не установлены')
+END
 
 # Запуск сервера
 python manage.py runserver 0.0.0.0:8000
